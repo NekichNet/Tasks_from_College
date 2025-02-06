c7006976bfa25ba1d03b9604e97bdd1c94af6c83
NikitaNet <102032304+NekichNet@users.noreply.github.com>
2025-02-07 01:39:25 +0500

    07_02 in progress

--- MathInt/MathRational.cpp
+++ MathInt/MathRational.cpp
@@ -10,7 +10,170 @@ MathRational::MathRational(double num) {
 		int last_num = (_denominator - ((_denominator / 10) * 10)).to_int();
 		zero_counter += last_num == 0 ? 1u : 0u;
 		nine_counter += std::abs(last_num) == 9 ? 1u : 0u;
-		if (nine_counter >= ROUNDING_THRESHOLD) { _denominator /= std::pow(10, 5); break; }
+		if (nine_counter >= ROUNDING_THRESHOLD) {
+			_denominator /= std::pow(10, ROUNDING_THRESHOLD);
+			break;
+		}
+		if (zero_counter >= ZERO_DENOM_THRESHOLD) {
+			_denominator /= std::pow(10, ZERO_DENOM_THRESHOLD);
+			break;
+		}
 	}
-	// TODO: Strip zeros.
+}
+
+void MathRational::setNumerator(MathInt numerator) { _numerator = numerator; }
+
+void MathRational::setDenominator(MathInt denominator) { _denominator = denominator; }
+
+void MathRational::simplify() {
+
+}
+
+void MathRational::inverse() { std::swap(_numerator, _denominator); }
+
+MathInt MathRational::getNumerator() const { return _numerator; }
+
+MathInt MathRational::getDenominator() const { return _denominator; }
+
+MathRational MathRational::getInverse() const { return MathRational(_denominator, _numerator); }
+
+MathInt MathRational::getUnits() const { return _numerator / _denominator; }
+
+bool MathRational::isCorrect() const { return _denominator > _numerator; }
+
+bool MathRational::isIncorrect() const { return _denominator <= _numerator; }
+
+bool MathRational::isPositive() const {
+	return _denominator.isPositive() == _numerator.isPositive();
+}
+
+bool MathRational::isNegative() const {
+	return _denominator.isPositive() != _numerator.isPositive();
+}
+
+bool MathRational::isSame(MathRational* other) const { return this == other; }
+
+MathRational operator+(MathRational a, MathRational b)
+{
+	unsigned max_num = a._units >= b._units ? a._units : b._units;
+	unsigned min_num = a._units + b._units - max_num;
+	return a._positive == b._positive ?
+		MathRational(static_cast<unsigned>(a._units + b._units), a._positive) :
+		MathRational(max_num - min_num, a._positive ? a._units >= b._units : b._units >= a._units);
+}
+
+MathRational operator-(MathRational a, MathRational b)
+{
+	return a + b.setSign(!b._positive);
+}
+
+MathRational operator*(MathRational a, MathRational b)
+{
+	return MathRational(a._units * b._units, a._positive == b._positive);
+}
+
+MathRational operator/(MathRational a, MathRational b)
+{
+	return MathRational(a._units / b._units, a._positive == b._positive);
+}
+
+MathRational operator%(MathRational a, MathRational b)
+{
+	return MathRational(a._units % b._units, a._positive == b._positive);
+}
+
+bool operator==(MathRational a, MathRational b)
+{
+	return a._units == b._units && a._positive == b._positive;
+}
+
+bool operator>=(MathRational a, MathRational b)
+{
+	return a._positive && b._positive ? a._units >= b._units :
+		!(a._positive || b._positive) ? a._units < b._units :
+		a._positive && !b._positive;
+}
+
+bool operator<=(MathRational a, MathRational b)
+{
+	return a._positive && b._positive ? a._units <= b._units :
+		!(a._positive || b._positive) ? a._units > b._units :
+	!a._positive && b._positive;
+}
+
+bool operator>(MathRational a, MathRational b)
+{
+	return a._positive && b._positive ? a._units > b._units :
+	!(a._positive || b._positive) ? a._units <= b._units :
+		a._positive && !b._positive;
+}
+
+bool operator<(MathRational a, MathRational b)
+{
+	return a._positive && b._positive ? a._units < b._units :
+		!(a._positive || b._positive) ? a._units >= b._units :
+		!a._positive && b._positive;
+}
+
+MathRational MathRational::operator+=(MathRational other)
+{
+	*this = *this + other;
+	return *this;
+}
+
+MathRational MathRational::operator-=(MathRational other)
+{
+	*this = *this - other;
+	return *this;
+}
+
+MathRational MathRational::operator*=(MathRational other)
+{
+	*this = *this * other;
+	return *this;
+}
+
+MathRational MathRational::operator/=(MathRational other)
+{
+	*this = *this / other;
+	return *this;
+}
+
+MathRational MathRational::operator%=(MathRational other)
+{
+	*this = *this % other;
+	return *this;
+}
+
+MathRational MathRational::operator++()
+{
+	*this += 1;
+	return MathRational(_units, _positive);
+}
+
+MathRational MathRational::operator++(int)
+{
+	MathRational num = MathRational(_units, _positive);
+	++*this;
+	return num;
+}
+
+MathRational MathRational::operator--()
+{
+	*this -= 1;
+	return MathRational(_units, _positive);
+}
+
+MathRational MathRational::operator--(int)
+{
+	MathRational num = MathRational(_units, _positive);
+	--*this;
+	return num;
+}
+
+std::ostream& operator<<(std::ostream& out, const MathRational& num)
+{
+	out << num._positive ? "" : "-";
+	out << num._units;
+	return out;
 }
--- MathInt/MathRational.h
+++ MathInt/MathRational.h
@@ -13,6 +13,50 @@ public:
 	MathRational(int numerator, int denominator = 1u) : _numerator(numerator), _denominator(denominator) {};
 	MathRational(double num);
 	MathRational(std::string numerator, std::string denominator) : _numerator(numerator), _denominator(denominator) {};
+
+	void setNumerator(MathInt numerator);
+	void setDenominator(MathInt denominator);
+	void simplify();
+	void inverse();
+
+	MathInt getNumerator() const;
+	MathInt getDenominator() const;
+	MathRational getInverse() const;
+	MathRational getSimplified() const;
+	MathInt getUnits() const;
+	bool isCorrect() const;
+	bool isIncorrect() const;
+	bool isPositive() const;
+	bool isNegative() const;
+	bool isSame(MathRational* other) const;
+
+	MathRational operator=(int num) { return MathRational(num); }
+	MathRational operator=(unsigned num) { return MathRational(num, true); }
+
+	friend MathRational operator+(MathRational a, MathRational b);
+	friend MathRational operator-(MathRational a, MathRational b);
+	friend MathRational operator*(MathRational a, MathRational b);
+	friend MathRational operator/(MathRational a, MathRational b);
+	friend MathRational operator%(MathRational a, MathRational b);
+
+	friend bool operator==(MathRational a, MathRational b);
+	friend bool operator>=(MathRational a, MathRational b);
+	friend bool operator<=(MathRational a, MathRational b);
+	friend bool operator>(MathRational a, MathRational b);
+	friend bool operator<(MathRational a, MathRational b);
+
+	MathRational operator+=(MathRational other);
+	MathRational operator-=(MathRational other);
+	MathRational operator*=(MathRational other);
+	MathRational operator/=(MathRational other);
+	MathRational operator%=(MathRational other);
+
+	MathRational operator++();
+	MathRational operator++(int);
+	MathRational operator--();
+	MathRational operator--(int);
+
+	friend std::ostream& operator<<(std::ostream& out, const MathRational& num);
 private:
 	MathInt _numerator;
 	MathInt _denominator;
