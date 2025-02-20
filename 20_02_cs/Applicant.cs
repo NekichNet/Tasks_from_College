4036840100a3369a8810b2862cb4d0c15d393f29
З - 16 < - 16@DESKTOP-8ICTQ04>
2025-02-20 15:22:27 +0500

    Some c sharp practice

---
+++ 20_02_cs/Applicant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_02_cs
+{
+	internal class Applicant
+	{
+		private string fullname, date;
+		private double average, extra;
+
+		public Applicant(string fullname, string date, double average, double extra)
+		{
+			this.fullname = fullname;
+			this.date = date;
+			this.average = average;
+			this.extra = extra;
+		}
+
+		public static bool operator >(Applicant left, Applicant right) {
+			return left.average == right.average ? left.extra > right.extra : left.average > right.average;
+		}
+
+        public static bool operator <(Applicant left, Applicant right)
+        {
+            return left.average == right.average ? left.extra < right.extra : left.average < right.average;
+        }
+
+        public static bool operator >(Applicant left, double num)
+        {
+            return left.average > num;
+        }
+
+        public static bool operator <(Applicant left, double num)
+        {
+            return left.average < num;
+        }
+    }
+}
+ 
---
+++ 20_02_cs/Deposit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_02_cs
+{
+	internal class Deposit
+	{
+		private static double percent = 0.05;
+
+        private string fullname;
+        private double money;
+
+		public static Deposit operator ++(Deposit deposit) {
+			deposit.money += deposit.money * percent; return deposit;
+		}
+
+		public static Deposit operator -(Deposit deposit, double num) { deposit.money -= num; return deposit; }
+
+		public static double GetPercent() { return percent; }
+	}
+}
