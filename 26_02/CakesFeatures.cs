640375f7f0767fcffdae513ee34eba96cbacf74d
NekichNet <nikitanekich2017@gmail.com>
2025-02-26 15:20:51 +0500

    some c sharp tasks done

---
+++ 26_02/CakesFeatures.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_02
+{
+    internal class CakesFeatures
+    {
+        private string _name;
+        private int _weight, _price, _calorie;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Price { get { return _price; } set { _price = value; } }
+        public int Calorie { get { return _calorie; } }
+
+        public string Content { get; }
+
+        public string Info { get { return $"Название: {Name}, Цена: {Price}," +
+                    $" Вес: {_weight}, Калорийность: {Calorie}, Состав: {Content}"; } }
+
+        public CakesFeatures(string name, int weight, int price, int calorie, string content)
+        {
+            _name = name;
+            _weight = weight;
+            _price = price;
+            _calorie = calorie;
+            Content = content;
+        }
+    }
+}
---
+++ 26_02/CakesMethods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _26_02
+{
+	internal class CakesMethods
+	{
+		private string _name, _content;
+		private int _weight, _price, _calorie;
+
+		public void SetName(string name) { _name = name; }
+		public void SetPrice(int price) { _price = price; }
+
+		public string GetName() { return _name; }
+		public int GetPrice() { return _price; }
+		public int GetCalorie() { return _calorie; }
+        public void SetContent(string content) { _content = content; }
+        public string GetContent() { return _content; }
+        public string GetInfo() { return $"Название: {_name}, Цена: {_price}, " +
+				$"Вес: {_weight}, Калорийность: {_calorie}, Состав: {_content}"; }
+        public CakesMethods(string name, string content, int weight, int price, int calorie)
+        {
+            _name = name;
+            _content = content;
+			_weight = weight;
+			_price = price;
+			_calorie = calorie;
+		}
+	}
+}
