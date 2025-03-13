5e1bb8d9a6a60eab57ec23264ad9b15e0fcb4f64
NekichNet <nikitanekich2017@gmail.com>
2025-03-13 15:06:34 +0500

    13_03 c# tasks done

---
+++ 13_03/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _13_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student("Настя", "ГД", new List<float>() { 4f, 3f, 3f, 2f, 4f }));
+            students.Add(new Student("Игорь", "РПО", new List<float>() { 5f, 3f, 2f, 2f, 4f }));
+            students.Add(new Student("Игорь2", "ГД", new List<float>() { 2f, 3f, 5f, 5f, 4f }));
+            students.Add(new Student("Света", "ГД", new List<float>() { 5f, 3f, 1f, 2f, 3f }));
+            students.Add(new Student("Дениска", "РПО", new List<float>() { 5f, 5f, 5f, 5f, 5f }));
+            students.Add(new Student("Галя", "РПО", new List<float>() { 4f, 4f, 4f, 5f, 4f }));
+            students.Add(new Student("Виталий", "ГД", new List<float>() { 3f, 4f, 5f, 4f, 3f }));
+
+            Dictionary<string, List<Student>> facs = new Dictionary<string, List<Student>>();
+
+            foreach (Student student in students) {
+                if (!facs.ContainsKey(student.Fac)) { facs.Add(student.Fac, new List<Student>()); }
+                facs[student.Fac].Add(student);
+            }
+
+            foreach (string fac in facs.Keys)
+            {
+                Console.WriteLine("Факультет: " + fac);
+                float sum = 0f;
+                Student best = null;
+                foreach (Student student in facs[fac]) {
+                    sum += student.Marks.Average();
+                    if (best == null) { best = student; }
+                    else if (student.Marks.Average() >= best.Marks.Average()) { best = student; }
+                }
+                Console.WriteLine("Средний балл: " + (sum / facs[fac].Count));
+                if (best != null)
+                    Console.WriteLine("Лучший студент: " + best.Name + " " + best.Marks.Average() + "\n");
+            }
+        }
+    }
+}
--- 27_02/Program.cs
+++ 27_02/Program.cs
@@ -1,10 +1,39 @@
 ﻿namespace _27_02
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, string> engToRus = new Dictionary<string, string>();
+			Dictionary<string, string> rusToEng = new Dictionary<string, string>();
+			engToRus.Add("Russia", "Россия");
+            engToRus.Add("USA", "США");
+            engToRus.Add("UK", "Великобритания");
+            engToRus.Add("China", "Китай");
+
+			foreach (string key in engToRus.Keys) {
+				rusToEng.Add(engToRus[key], key);
+			}
+
+			string choice = "";
+			Console.Write("С какого языка перевод: ");
+			choice = Console.ReadLine();
+			string country = "";
+            Console.Write("Введите название страны: ");
+            country = Console.ReadLine();
+
+            switch (choice)
+			{
+				case "ru":
+					Console.WriteLine(rusToEng[country]);
+					break;
+				case "eng":
+                    Console.WriteLine(engToRus[country]);
+					break;
+                default:
+					Console.WriteLine("Ошибка");
+					break;
+			}
         }
-    }
+	}
 }
