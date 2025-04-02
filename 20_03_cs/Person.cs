24151d842667e2c759f67afd0802787cc51668a1
NekichNet <nikitanekich2017@gmail.com>
2025-04-02 15:17:27 +0500

    02_04 c#

---
+++ 20_03_cs/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _20_03_cs
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Lastname { get; set; }
+
+        public int Age { get; set; }
+
+        public Person() { }
+
+        public Person(string name, string surname, string lastname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Lastname = lastname;
+            Age = age;
+        }
+
+        public void Fullname()
+        {
+            Console.WriteLine(Name + " " + Surname + " " + Lastname);
+        }
+    }
+}
--- 20_03_cs/Program.cs
+++ 20_03_cs/Program.cs
@@ -1,14 +1,48 @@
-﻿namespace _20_03_cs
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace _20_03_cs
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Library<Media> lib = new Library<Media>();
-			lib.Add(new Book(14, "meme", "MyMeme123", "me", 1954, true));
-            lib.Add(new MusicAlbum(50, "my cousin", "Trash", "my cousin", 2001, false));
-			lib.PrintAll();
-			Console.WriteLine(((List<Media>)lib.FilterByYear(1954))[0].GetInfo());
+			//Library<Media> lib = new Library<Media>();
+			//lib.Add(new Book(14, "meme", "MyMeme123", "me", 1954, true));
+			//         lib.Add(new MusicAlbum(50, "my cousin", "Trash", "my cousin", 2001, false));
+			//lib.PrintAll();
+			//Console.WriteLine(((List<Media>)lib.FilterByYear(1954))[0].GetInfo());
+
+			//var per = new Person("Vova", "Kovbaskus", "Kakoytotam", 20);
+			//XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+			//using(StreamWriter sw = new StreamWriter("kovbaskus.xml"))
+			//{
+			//	xmlSerializer.Serialize(sw, per);
+			//}
+
+			//XDocument doc = XDocument.Load("kovbaskus.xml");
+			//var persons = doc.Descendants("Person");
+			//foreach (var item in persons)
+			//{
+			//	Console.WriteLine(item.Element("Name")?.Value);
+   //             Console.WriteLine(item.Element("Surname")?.Value);
+   //             Console.WriteLine(item.Element("Lastname")?.Value);
+   //             Console.WriteLine(item.Element("Age")?.Value);
+   //         }
+
+			XmlDocument doc = new XmlDocument();
+			XmlElement company = doc.CreateElement("Company");
+			XmlElement employee = doc.CreateElement("Employee");
+			employee.SetAttribute("Id", "1");
+			employee.AppendChild(doc.CreateElement("Name", "John"));
+            employee.AppendChild(doc.CreateElement("Age", "20"));
+            company.AppendChild(employee);
+            company.AppendChild(employee);
+            company.AppendChild(employee);
+            doc.AppendChild(company);
+            doc.Save("document.xml");
         }
 	}
 }
