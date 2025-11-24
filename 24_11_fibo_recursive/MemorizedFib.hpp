8ae989995278a58c46691ea659cd106eb1e26975
tramplergaming <evgen.medelyan5961@gmail.com>
2025-11-24 12:49:57 +0500

    fibo moved to own file

---
+++ 24_11_fibo_recursive/MemorizedFib.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<vector>
+
+static class MemorizedFib {
+public:
+	static int find(int n) {
+		if (n < 1) throw "Error: n < 1";
+		if (_memo.size() < n - 1) _memo.resize(n + 1, 0);
+		if (n <= 2) return 1;
+		if (_memo[n] > 0) return _memo[n];
+		_memo[n] = find(n - 1) + find(n - 2);
+		return _memo[n];
+	}
+private:
+	static std::vector<int> _memo;
+};
+std::vector<int> MemorizedFib::_memo;
